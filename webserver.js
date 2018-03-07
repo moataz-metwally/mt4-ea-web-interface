31aa8ff2335df37312b10b4aaff7ade58e070110
Moataz Metwally <moataz.metwally@ict.nl>
2018-03-07 17:18:13 +0100

    +removing the EA specific button. +adding json response which will be check by the mt4 ea every 3 seconds. +handling the the four buttons start, stop, Close all orders, Close all pending orders.

--- webserver.js
+++ webserver.js
@@ -1,14 +1,64 @@
 var express = require("express");
 var app     = express();
 var path    = require("path");
+var bodyParser = require('body-parser');
 
-var soso = 6;
+
+var status = 0;
+var close_all_orders = 0;
+var close_all_pending_orders = 0;
+
 app.engine('html', require('ejs').renderFile)
-app.set('view engine', 'ejs')
-app.get('/ea-path',function(req,res){
+app.set('view engine', 'ejs');
+app.use(bodyParser.json()); 
+app.use(bodyParser.urlencoded({ extended: true })); 
 
-    console.log( req.query.toto);
+
+
+
+
+app.get('/ea-check',function(req,res){
+
+
+    var outObj = {
+        status: status, 
+        close_all_orders: close_all_orders,
+        close_all_pending_orders: close_all_pending_orders
+    }
     
+    close_all_orders = 0;
+    close_all_pending_orders = 0;
+    res.send(JSON.stringify(outObj));	//Write the response
+});
+
+
+app.get('/ea-web',function(req,res){
+    
+
+        res.render('web_ea.html',{title:req.query.toto});
+    });
+
+app.post('/ea-path',function(req,res){
+    
+if(req.body.start){
+    status = 1 ;
+}else if(req.body.stop){
+
+    status = 0 ;
+}
+else if(req.body.close_all_orders){
+
+    close_all_orders = 1;
+}
+else if(req.body.close_all_pendings){
+
+    close_all_pending_orders = 1;
+}
+
+  console.log("Status:"+status+"\r\n");
+  console.log("close_all_orders:"+close_all_orders+"\r\n");
+  console.log("close_all_pending_orders:"+close_all_pending_orders+"\r\n");
+
     res.render('web_ea.html',{title:req.query.toto});
 });
 
